04afb043a62284096d6210033a562e84ddd9b1d8
Safwan3528 <tokrimauhitam@gmail.com>
2024-09-18 23:04:07 +0800

    safwan nextjs portfolio

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -144,7 +144,7 @@ export function PortfolioComponent() {
                 Get in touch
               </a>
               <a
-                href="/path-to-your-resume.pdf"
+                href="/resume/Resume.pdf"
                 download
                 className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600 transition duration-300 flex items-center"
               >
